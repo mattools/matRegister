c5a093ebc43e3547a9daf5f9d4e5e3fc300a4fe4
dlegland <david.legland@inra.fr>
2013-03-18 16:16:57 +0000

    add Contents.m

---
+++ oolip/Contents.m
@@ -0,0 +1,23 @@
+%OOLIP Image Registration Toolkit
+%
+%   Contains various classes for image transform, interpolation, sampling,
+%   and registration.
+%
+%   modules:
+%   imageSamplers   - For sampling pixels within images
+%   interpolators   - Interpolation of image values
+%   metrics         - Compare two images
+%   optimizers      - Some optimization algorithms
+%   transforms      - Several transform models, in 2D/3D
+%   utils           - Utility for interoperability with other libs
+%
+%   See also
+%     Image
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2013-03-18,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2013 INRA - Cepia Software Platform.
+
+help(mfilename);
