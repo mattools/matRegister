f63cb6421a2a92fd38f9e17eac87d3d8b17a6489
dlegland <david.legland@inra.fr>
2017-09-29 09:32:37 +0200

    add AffineTransformModel2D.m

---
+++ matRegister/transforms/AffineTransformModel2D.m
@@ -0,0 +1,138 @@
+classdef AffineTransformModel2D < AffineTransform & ParametricTransform 
+%Transformation model for a 2D affine transform
+%
+%   TRANSFO = AffineTransformModel2D()
+%   Creates a new instanfce of affine parametric model in 2D, with 6
+%   parameters.
+%
+%   Example
+%   AffineTransformModel2D
+%
+%   See also
+%
+
+% ------
+% Author: David Legland
+% e-mail: david.legland@inra.fr
+% Created: 2017-09-29,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2017 INRA - Cepia Software Platform.
+
+
+%% Constructors
+methods
+    function this = AffineTransformModel2D(varargin)
+        % Create a new affine transform model
+        
+        % initialize to identity
+        this.params = [1 0 0   0 1 0];
+        
+        if ~isempty(varargin)
+            % extract first argument, and try to interpret
+            var = varargin{1};
+            if isa(var, 'AffineTransformModel2D')
+                % copy constructor
+                this.params = var.params;
+                
+            elseif isnumeric(var)
+                len = length(var);
+                if len == 6
+                    % all parameters are specified as a row vector
+                    this.params = var;
+                    
+                elseif sum(size(var)~=[3 3])==0 || sum(size(var)~=[2 3])==0
+                    % the parameter matrix is specified
+                    var = var(1:2, :)';
+                    this.params = var(:)';
+                    
+                elseif len == 1
+                    % give the possibility to call constructor with the
+                    % dimension of image
+                    % Initialize with identity transform
+                    if var ~= 2
+                        error('Defined only for 2 dimensions');
+                    end
+                    
+                else
+                    error('Please specify affine parameters');
+                end
+                
+            else
+                error('Unable to understand input arguments');
+            end
+        end
+        
+        % setup parameter names
+        this.paramNames = {...
+            'm00', 'm01', 'm02', ...
+            'm10', 'm11', 'm12', ...
+            };
+    end     
+    
+end
+
+%% Methods specific to this class
+methods
+    function initFromAffineTransform(this, transform)
+        % Initialize parameters from an affine transform (class or matrix)
+        %
+        % Example
+        %   T = AffineTransformModel2D();
+        %   mat = createRotation([10 20], .3);
+        %   T.initFromAffineTransform(mat);
+        %   T.getParameters()
+        %
+        
+        % if the first argument is a Transform, extract its affine matrix
+        if isa(transform, 'AffineTransform')
+            transform = getAffineMatrix(transform);
+        end
+        
+        % format matrix to have a row vector of 12 elements
+        params0 = transform(1:3, :)';
+        this.params = params0(:)';
+    end
+
+end
+
+
+%% Implementation of methods inherited from AffineTransform
+methods
+    function dim = getDimension(this) %#ok<MANU>
+        % Returns the dimension of the tansform, in this case 2.
+        dim = 2;
+    end
+
+    function mat = getAffineMatrix(this)
+        % Compute affine matrix associated with this transform
+        
+        % convert parameters to a 3-by-3 affine matrix
+        mat = [this.params(1:3); this.params(4:6); 0 0 1];
+    end
+end
+
+
+%% Implementation of methods inherited from ParametricTransform
+methods
+    function jacobian = getParametricJacobian(this, x, varargin) %#ok<INUSL>
+        % Compute jacobian matrix, i.e. derivatives for each parameter
+        % Result is a 2-by-6 matrix
+        
+        % extract coordinate of input point(s)
+        if isempty(varargin)
+            y = x(:,2);
+            x = x(:,1);
+        else
+            y = varargin{1};
+        end
+        
+        % compute the Jacobian matrix using pre-computed elements
+        jacobian = [...
+            x y 1       zeros(1,3); ...
+            zeros(1,3)  x y 1; ...
+            ];
+        
+    end
+    
+end % parametric transform methods 
+
+end
