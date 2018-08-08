b476bbedcafa159b5f9268332cecf1660c3d4349
dlegland <david.legland@inra.fr>
2018-08-08 18:40:24 +0200

    add secondDerivatives

--- matRegister/transforms/QuadPolynomialTransformModel2D.m
+++ matRegister/transforms/QuadPolynomialTransformModel2D.m
@@ -7,6 +7,11 @@ classdef QuadPolynomialTransformModel2D < ParametricTransform
 %   x' = P1 + P3 * x + P5 * y + P7 * x^2 +  P9 * x*y + P11 * y^2
 %   y' = P2 + P4 * x + P6 * y + P8 * x^2 + P10 * x*y + P12 * y^2
 %
+%   Example
+%   % initialize identity
+%   T = QuadPolynomialTransformModel2D([0 0 1 0 0 1 zeros(1,6)]);
+%   % init to a non-affine transform
+%   T = QuadPolynomialTransformModel2D([50 40 1 0 0 1  [1 -2 3 -1 2 -3]*1e-3]);
 %
 %   See also
 %
@@ -55,8 +60,36 @@ methods
 
 end % end constructors
 
+%% Methods specific to the class
+methods
+    function deriv = secondDerivatives(this, indI, indJ, point)
+        % compute the value of second derivatives for the given point(s)
+        %
+        % D2 = secondDerivatives(T, INDI, INDJ, POINT)
+        % Return a M-by-2 array, with as many rows as the number of points.
+        % First columns is the second derivative of the x-transform part,
+        % and second column is the second dervative of the y-transform
+        % part.
+        
+        % for quadratic transforms, second derivatives are constants
+        if indI == 1 && indJ == 1
+            deriv = 2*this.params([7 8]);
+        elseif indI == 2 && indJ == 2
+            deriv = 2*this.params([11 12]);
+        elseif (indI == 1 && indJ == 2) || (indI == 2 && indJ == 1)
+            deriv = this.params([9 10]);
+        else
+            error('Derivative indices must be 1 or 2');
+        end
+        
+        % normalize size of output with number of points
+        if size(point, 1) > 1
+            deriv = bsxfun(@times, deriv, ones(size(point,1), 1));
+        end
+    end
+end
 
-%% Methods
+%% Methods implementing the Transform interface
 methods
     function pointT = transformPoint(this, point)
         
@@ -85,11 +118,11 @@ methods
         pointT = [x2 y2];
     end
     
-    function  getParametricJacobian(this, x, varargin)
+    function getParametricJacobian(this, x, varargin)
         % Compute jacobian matrix, i.e. derivatives for each parameter
     end
     
-    function  jacobian = getJacobian(this, point)
+    function jacobian = getJacobian(this, point)
         % Computes jacobian matrix, i.e. derivatives wrt to each coordinate
         % jacob(i,j) = d x_i / d x_j
         
@@ -115,6 +148,7 @@ methods
         % Compute jacobian matrix, i.e. derivatives for each parameter
         dim = 2;
     end
+    
 end % end methods
 
 end % end classdef
